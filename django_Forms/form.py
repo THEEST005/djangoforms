f477d346d9cbd012dedf12bcfbac62e8d41c6972
eMobilis <elikibetael4@gmail.com>
2023-02-21 12:10:59 +0300

    created forms

---
+++ django_Forms/form.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class UserReg( forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
+    password = forms.CharField()
+
+
+
